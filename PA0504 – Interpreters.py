9497b6eee4d7a74cb162940e2f424bb83f5a3084
Andile Andre3000 <lasmkhwanazi@gmail.com>
2026-07-27 17:27:33 +0100

    Add files via upload

---
+++ PA0504 – Interpreters.py
@@ -0,0 +1,21 @@
+'''
+#1. What is an interpreter?
+//An interpreter is a program that reads and executes
+ code line by line during runtime. Unlike a compiler, 
+ which translates the entire program into machine code before execution, 
+ an interpreter processes code sequentially, 
+ translating each instruction to machine code as it encounters it.
+
+#2. What role does Node.js play in JavaScript execution?
+//Node.js is a JavaScript runtime environment that plays a crucial role in 
+//JavaScript execution by enabling JavaScript code to run outside of web browsers on servers and computers.
+
+
+#3. Does the interpreter continue after the error?
+//No. The interpreter stops executing the program when it encounters a syntax error.
+
+#4. What message appears?
+//Answer: A SyntaxError message appears, indicating the line where the error occurred 
+//and describing the problem (for example, SyntaxError: Unexpected token or SyntaxError: Unexpected end of input).'''
+
+
